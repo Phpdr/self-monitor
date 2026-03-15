96475622120e0796176059f973b3082d4b86e04c
Phabrício Pedro Alves <114709921+Phpdr@users.noreply.github.com>
2026-03-15 06:55:22 -0300

    Update VNC proxy commands in iniciar_self_monitor.bat

--- iniciar_self_monitor.bat
+++ iniciar_self_monitor.bat
@@ -10,12 +10,12 @@ timeout /t 3 >nul
 
 echo Iniciando VNC proxies...
 
-start powershell -NoExit -Command "py -3.11 -m websockify 6080 "IP DA CONEXÃO"--web C:\self-monitor\novnc"
-start powershell -NoExit -Command "py -3.11 -m websockify 6081 "IP DA CONEXÃO" --web C:\self-monitor\novnc"
-start powershell -NoExit -Command "py -3.11 -m websockify 6082 "IP DA CONEXÃO" --web C:\self-monitor\novnc"
-start powershell -NoExit -Command "py -3.11 -m websockify 6083 "IP DA CONEXÃO" --web C:\self-monitor\novnc"
-start powershell -NoExit -Command "py -3.11 -m websockify 6084 "IP DA CONEXÃO" --web C:\self-monitor\novnc"
-start powershell -NoExit -Command "py -3.11 -m websockify 6085 "IP DA CONEXÃO" --web C:\self-monitor\novnc"
+start powershell -NoExit -Command "py -3.11 -m websockify "PORTA DA CONEXÃO" "IP DA CONEXÃO"--web C:\self-monitor\novnc"
+start powershell -NoExit -Command "py -3.11 -m websockify "PORTA DA CONEXÃO" "IP DA CONEXÃO" --web C:\self-monitor\novnc"
+start powershell -NoExit -Command "py -3.11 -m websockify "PORTA DA CONEXÃO" "IP DA CONEXÃO" --web C:\self-monitor\novnc"
+start powershell -NoExit -Command "py -3.11 -m websockify "PORTA DA CONEXÃO" "IP DA CONEXÃO" --web C:\self-monitor\novnc"
+start powershell -NoExit -Command "py -3.11 -m websockify "PORTA DA CONEXÃO" "IP DA CONEXÃO" --web C:\self-monitor\novnc"
+start powershell -NoExit -Command "py -3.11 -m websockify "PORTA DA CONEXÃO" "IP DA CONEXÃO" --web C:\self-monitor\novnc"
 
 echo Sistema iniciado
 pause
